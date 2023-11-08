a60c36b88ce3662b5bc477ff3e95a4dc91bbda5b
Julian Riedinger <julianriedinger27@gmail.com>
2023-11-08 15:37:52 -0300

    removed afterClass

--- src/test/java/tests/UserTests.java
+++ src/test/java/tests/UserTests.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
